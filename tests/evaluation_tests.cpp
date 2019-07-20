e9c96f446683a1526e91c6ffcd42eb1be5f95e5b
Anton94_home <antonvdudov@gmail.com>
2019-07-20 16:40:20 +0300

    Add a few tests for brute force evaluation formulas with constants

--- tests/evaluation_tests.cpp
+++ tests/evaluation_tests.cpp
@@ -155,3 +155,393 @@ TEST_CASE("satisfiable evaluation 4", "[brute force evaluation]")
         })"_json,
         false);
 }
+
+TEST_CASE("satisfiable evaluation with constant 0", "[brute force evaluation]")
+{
+    // C(1, b) & ~C(a, 0)
+    check_for_satisfiable_evaluation(
+        R"({
+            "name": "conjunction",
+            "value": [
+               {
+                  "name": "contact",
+                  "value": [
+                     {
+                        "name": "constant_1"
+                     },
+                     {
+                        "name": "string",
+                        "value": "b"
+                     }
+                  ]
+               },
+               {
+                  "name": "negation",
+                  "value": {
+                     "name": "contact",
+                     "value": [
+                        {
+                           "name": "string",
+                           "value": "a"
+                        },
+                        {
+                           "name": "constant_0"
+                        }
+                     ]
+                  }
+               }
+            ]
+        })"_json,
+        true);
+}
+
+TEST_CASE("satisfiable evaluation with constant 1", "[brute force evaluation]")
+{
+    // (C(a, b) & ~C(c, 0)) & <=(1, 0)
+    check_for_satisfiable_evaluation(
+        R"({
+           "name": "conjunction",
+           "value": [
+              {
+                 "name": "conjunction",
+                 "value": [
+                    {
+                       "name": "contact",
+                       "value": [
+                          {
+                             "name": "string",
+                             "value": "a"
+                          },
+                          {
+                             "name": "string",
+                             "value": "b"
+                          }
+                       ]
+                    },
+                    {
+                       "name": "negation",
+                       "value": {
+                          "name": "contact",
+                          "value": [
+                             {
+                                "name": "string",
+                                "value": "c"
+                             },
+                             {
+                                "name": "constant_0"
+                             }
+                          ]
+                       }
+                    }
+                 ]
+              },
+              {
+                 "name": "less",
+                 "value": [
+                    {
+                        "name": "constant_1"
+                    },
+                    {
+                       "name": "constant_0"
+                    }
+                 ]
+              }
+           ]
+        })"_json,
+        false);
+}
+
+TEST_CASE("satisfiable evaluation with constant 2", "[brute force evaluation]")
+{
+    // (C(a, b) & ~C(c, 1)) & <=(1, a)
+    check_for_satisfiable_evaluation(
+        R"({
+           "name": "conjunction",
+           "value": [
+              {
+                 "name": "conjunction",
+                 "value": [
+                    {
+                       "name": "contact",
+                       "value": [
+                          {
+                             "name": "string",
+                             "value": "a"
+                          },
+                          {
+                             "name": "string",
+                             "value": "b"
+                          }
+                       ]
+                    },
+                    {
+                       "name": "negation",
+                       "value": {
+                          "name": "contact",
+                          "value": [
+                             {
+                                "name": "string",
+                                "value": "c"
+                             },
+                             {
+                                "name": "constant_1"
+                             }
+                          ]
+                       }
+                    }
+                 ]
+              },
+              {
+                 "name": "less",
+                 "value": [
+                    {
+                       "name": "constant_1"
+                    },
+                    {
+                       "name": "string",
+                       "value": "a"
+                    }
+                 ]
+              }
+           ]
+        })"_json,
+        true);
+}
+
+TEST_CASE("satisfiable evaluation with constant 3", "[brute force evaluation]")
+{
+    // (C(a, b) & ~C(c, 1)) & T
+    check_for_satisfiable_evaluation(
+        R"({
+           "name": "conjunction",
+           "value": [
+              {
+                 "name": "conjunction",
+                 "value": [
+                    {
+                       "name": "contact",
+                       "value": [
+                          {
+                             "name": "string",
+                             "value": "a"
+                          },
+                          {
+                             "name": "string",
+                             "value": "b"
+                          }
+                       ]
+                    },
+                    {
+                       "name": "negation",
+                       "value": {
+                          "name": "contact",
+                          "value": [
+                             {
+                                "name": "string",
+                                "value": "c"
+                             },
+                             {
+                                "name": "constant_1"
+                             }
+                          ]
+                       }
+                    }
+                 ]
+              },
+              {
+                 "name": "constant_T"
+              }
+           ]
+        })"_json,
+        true);
+}
+
+TEST_CASE("satisfiable evaluation with constant 4", "[brute force evaluation]")
+{
+    // (C(a, b) & ~C(c, 1)) & F
+    check_for_satisfiable_evaluation(
+        R"({
+           "name": "conjunction",
+           "value": [
+              {
+                 "name": "conjunction",
+                 "value": [
+                    {
+                       "name": "contact",
+                       "value": [
+                          {
+                             "name": "string",
+                             "value": "a"
+                          },
+                          {
+                             "name": "string",
+                             "value": "b"
+                          }
+                       ]
+                    },
+                    {
+                       "name": "negation",
+                       "value": {
+                          "name": "contact",
+                          "value": [
+                             {
+                                "name": "string",
+                                "value": "c"
+                             },
+                             {
+                                "name": "constant_1"
+                             }
+                          ]
+                       }
+                    }
+                 ]
+              },
+              {
+                 "name": "constant_F"
+              }
+           ]
+        })"_json,
+        false);
+}
+
+TEST_CASE("satisfiable evaluation with constant 5", "[brute force evaluation]")
+{
+    // (C(a, b) & ~C(c, 1)) | F
+    check_for_satisfiable_evaluation(
+        R"({
+           "name": "disjunction",
+           "value": [
+              {
+                 "name": "conjunction",
+                 "value": [
+                    {
+                       "name": "contact",
+                       "value": [
+                          {
+                             "name": "string",
+                             "value": "a"
+                          },
+                          {
+                             "name": "string",
+                             "value": "b"
+                          }
+                       ]
+                    },
+                    {
+                       "name": "negation",
+                       "value": {
+                          "name": "contact",
+                          "value": [
+                             {
+                                "name": "string",
+                                "value": "c"
+                             },
+                             {
+                                "name": "constant_1"
+                             }
+                          ]
+                       }
+                    }
+                 ]
+              },
+              {
+                 "name": "constant_F"
+              }
+           ]
+        })"_json,
+        true);
+}
+
+TEST_CASE("satisfiable evaluation with constant 6", "[brute force evaluation]")
+{
+    // (C(0, b) & ~C(c, 1)) | F
+    check_for_satisfiable_evaluation(
+        R"({
+           "name": "disjunction",
+           "value": [
+              {
+                 "name": "conjunction",
+                 "value": [
+                    {
+                       "name": "contact",
+                       "value": [
+                          {
+                               "name": "constant_0"
+                          },
+                          {
+                             "name": "string",
+                             "value": "b"
+                          }
+                       ]
+                    },
+                    {
+                       "name": "negation",
+                       "value": {
+                          "name": "contact",
+                          "value": [
+                             {
+                                "name": "string",
+                                "value": "c"
+                             },
+                             {
+                                "name": "constant_1"
+                             }
+                          ]
+                       }
+                    }
+                 ]
+              },
+              {
+                 "name": "constant_F"
+              }
+           ]
+        })"_json,
+        false);
+}
+
+TEST_CASE("satisfiable evaluation with constant 7", "[brute force evaluation]")
+{
+    // (C(0, b) & ~C(c, 1)) | T
+    check_for_satisfiable_evaluation(
+        R"({
+           "name": "disjunction",
+           "value": [
+              {
+                 "name": "conjunction",
+                 "value": [
+                    {
+                       "name": "contact",
+                       "value": [
+                          {
+                             "name": "constant_0"
+                          },
+                          {
+                             "name": "string",
+                             "value": "b"
+                          }
+                       ]
+                    },
+                    {
+                       "name": "negation",
+                       "value": {
+                          "name": "contact",
+                          "value": [
+                             {
+                                "name": "string",
+                                "value": "c"
+                             },
+                             {
+                                "name": "constant_1"
+                             }
+                          ]
+                       }
+                    }
+                 ]
+              },
+              {
+                 "name": "constant_T"
+              }
+           ]
+        })"_json,
+        true);
+}
