873ecd9b13b0e770c83191c0edb2f00e48d6ed40
Anton94 <antonvdudov@gmail.com>
2019-07-25 16:32:11 +0300

    Add "ERROR: " prefix for the error logger in tests and formula_proover app

--- tests/source.cpp
+++ tests/source.cpp
@@ -11,7 +11,7 @@ int main(int argc, char* argv[])
     // If some test fails, enable the loggers for more info
     set_trace_logger([](std::stringstream&& s) { std::cout << s.rdbuf() << std::endl; });
     set_info_logger([](std::stringstream&& s) { std::cout << s.rdbuf() << std::endl; });
-    set_error_logger([](std::stringstream&& s) { std::cerr << s.rdbuf() << std::endl; });
+    set_error_logger([](std::stringstream&& s) { std::cerr << "ERROR: " << s.rdbuf() << std::endl; });
 
     int result = Catch::Session().run(argc, argv);
     // potential teardown
