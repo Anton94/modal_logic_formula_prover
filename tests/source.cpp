b3ee88e67d8f191a4fb8da5e7525819be3ee95de
Anton94 <antonvdudov@gmail.com>
2019-07-26 17:43:40 +0300

    Add a little test for moving formula_mgr object
    mainly to be ran with a sanitizer, e.g. leak to detect potential leaks, etc...

--- tests/source.cpp
+++ tests/source.cpp
@@ -17,3 +17,92 @@ int main(int argc, char* argv[])
     // potential teardown
     return result;
 }
+
+// TODO make common file for such tests
+TEST_CASE("move assignment of formula_mgr", "[run_with_sanitizer]")
+{
+    // (C(a, b) & ~C(c, 0)) & <=(1, 0)
+    auto complex_formula_json =
+        R"({
+           "name": "conjunction",
+           "value": [
+              {
+                 "name": "conjunction",
+                 "value": [
+                    {
+                       "name": "contact",
+                       "value": [
+                          {
+                             "name": "string",
+                             "value": "a"
+                          },
+                          {
+                             "name": "string",
+                             "value": "b"
+                          }
+                       ]
+                    },
+                    {
+                       "name": "negation",
+                       "value": {
+                          "name": "contact",
+                          "value": [
+                             {
+                                "name": "string",
+                                "value": "c"
+                             },
+                             {
+                                "name": "string",
+                                "value": "0"
+                             }
+                          ]
+                       }
+                    }
+                 ]
+              },
+              {
+                 "name": "equal0",
+                 "value": {
+                    "name": "Tand",
+                    "value": [
+                       {
+                          "name": "constant_1"
+                       },
+                       {
+                          "name": "Tstar",
+                          "value": {
+                              "name": "constant_0"
+                          }
+                       }
+                    ]
+                 }
+              }
+           ]
+        })"_json;
+    formula_mgr mgr;
+
+    mgr.build(complex_formula_json);
+
+    // C(a, b)
+    auto simple_formula_json =
+        R"({
+        "name": "contact",
+        "value": [
+           {
+              "name": "string",
+              "value": "a"
+           },
+           {
+              "name": "string",
+              "value": "b"
+           }
+        ]
+    })"_json;
+    auto simple_formula_json_copy = simple_formula_json;
+
+    formula_mgr mgr2;
+    mgr2.build(simple_formula_json);
+    mgr2 = std::move(mgr);
+
+    formula_mgr mgr3 = std::move(mgr2);
+}
