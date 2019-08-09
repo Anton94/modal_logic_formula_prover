0c0e240c81ecd6967551f3dcdd747bfbd673fe00
Anton94 <antonvdudov@gmail.com>
2019-08-09 17:26:13 +0300

    [tools] Add random word generation option of the cfg_language_generator
    Note that if the length of the words is not enough it might get stuck
    
    Usage: add -r 42 to generate 42 random words from the language

--- tools/cfg_language_generator.py
+++ tools/cfg_language_generator.py
@@ -1,4 +1,5 @@
 import sys
+import random
 from optparse import OptionParser
 
 rules = {} # TODO: make them arguments
@@ -52,6 +53,34 @@ def generate_language(length, output_filename):
                 out.write(s + '\n')
                 #print(s)
 
+def generate_random_words(length, count, output_filename):
+    non_terminals = rules.keys()
+
+    print("Non-terminals: ", end='')
+    print(*non_terminals, sep=', ')
+
+    out = open(output_filename,"w+")
+
+    print("Generating " + str(count) + " words with length up to " + str(length) + "...")
+    while count > 0:
+        worklist = [start_symbol]
+        while worklist:
+            s = worklist.pop(0)
+
+            if length_without_sapces(s) <= length:
+                found_non_terminal = False
+                for i, symbol in enumerate(s):
+                    if symbol in non_terminals:
+                        found_non_terminal = True
+                        rule = random.choice(rules[symbol])
+                        worklist.append(s[:i] + ' '.join(rule) + s[i + 1:])
+                        break
+
+                if not found_non_terminal:
+                    out.write(s + '\n')
+                    count = count - 1
+                    #print(s)
+
 if __name__ == "__main__":
     parser = OptionParser()
     parser.add_option("-i", "--cfg_filename", dest="cfg_filename", type = "string",
@@ -62,10 +91,13 @@ if __name__ == "__main__":
                       "    F = x"
                       "    F = y"
                       "    F = var")
-    parser.add_option("-l", "--length", dest="length", default = 5, type = "int",
-                      help="The generated word's max character length (without spaces)")
     parser.add_option("-o", "--output_filename", dest="output_filename", type = "string", default = "out_cfg_language.txt",
                       help="Filename for the generated language words")
+    parser.add_option("-l", "--length", dest="length", default = 10, type = "int",
+                      help="The generated word's max character length (without spaces)")
+    parser.add_option("-r", "--random_words_count", dest="random_words_count", default = 0, type = "int",
+                      help="If bigger than 0 it will generate random words of that ammount instead of all words. "
+                      "Make sure that the length is big enough or it might not be able to generate the words and hold")
 
     options, args = parser.parse_args()
     print(options)
@@ -81,4 +113,7 @@ if __name__ == "__main__":
         print(lhs + ' = ', end = '')
         print(*rules[lhs], sep=',')
 
-    generate_language(options.length, options.output_filename)
+    if options.random_words_count > 0:
+        generate_random_words(options.length, options.random_words_count, options.output_filename)
+    else:
+        generate_language(options.length, options.output_filename)
