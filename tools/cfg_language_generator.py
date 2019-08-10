6268b5aae9f636c057cfd61deedc151667787610
Anton94_laptop <antonvdudov@gmail.com>
2019-08-10 14:41:23 +0300

    [tools] Add (optional) probabilities (between 0 and 1) for the CFG rules
    The probabilities are used in ranodom word generation which indicates what is the chance to choose that rule
    
    Usage:
    S 0.5 = F & F
    S     = F | F
    S     = -F
    F     = a
    F     = b
    F 0.1 = 0
    
    The missing probabilities are autocalculated in the following:
    
    S 0.5  = F & F
    S 0.25 = F | F
    S 0.25 = -F
    F 0.45 = a
    F 0.45 = b
    F 0.1  = 0

--- tools/cfg_language_generator.py
+++ tools/cfg_language_generator.py
@@ -1,28 +1,83 @@
 import sys
 import random
+import math
 from optparse import OptionParser
 
 rules = {} # TODO: make them arguments
 start_symbol = None
+
+def print_grammar():
+    print('Grammar start_symbol is ' + start_symbol)
+    print("Non-terminals: ", end='')
+    print(*rules.keys(), sep=', ')
+    print('Rules: ')
+    for lhs in rules:
+        print(lhs + ' = ', end = '')
+        print(*rules[lhs], sep=',')
 
 def load_grammar(filename):
     global start_symbol
     global rules
 
     for line in open(filename, 'r'):
+        # the format is of the following type: S 0.5 = K M L
+        # where S is the non terminal, 0.5 is optional probability factor
+        # for choosing that rule when generating a random word from the language
+        # and K M L is right hand side of the rule
         rule_components = line.strip('\n').split(' ')
         lhs = rule_components[0]
-        # rule_components[1] should be '='
-        rhs = rule_components[2:]
+        probability = None
+        if rule_components[1] == '=':
+            rhs = rule_components[2:]
+        else:
+            probability = rule_components[1]
+            rhs = rule_components[3:]
 
         # the start symbol is the first rule's lhs symbol
         if start_symbol is None:
             start_symbol = lhs
 
         if lhs in rules:
-            rules[lhs].append(rhs)
+            rules[lhs].append((probability, rhs))
         else:
-            rules[lhs] = [rhs]
+            rules[lhs] = [(probability, rhs)]
+
+    # autofill the missing probabilities
+    for non_terminal, productions in rules.items():
+        # productions is of type [(probability, rhs), ...]
+        accumulated_probability = 0
+        productions_without_probability = 0
+        for i, (probability, production) in enumerate(productions):
+            if probability == None:
+                productions_without_probability = productions_without_probability + 1
+            else:
+                float_probability = round(float(probability), 3)
+                productions[i] = (float_probability, production)
+                accumulated_probability = accumulated_probability + float_probability
+
+        if accumulated_probability > 1:
+            print("The non_terminal " + non_terminal + " rules has combined probability of more than 1")
+            exit()
+
+        if  productions_without_probability > 0:
+            missing_probability = (1 - accumulated_probability) / productions_without_probability
+            for i, (probability, production) in enumerate(productions):
+                if probability == None:
+                    productions[i] = (missing_probability, production)
+
+    print_grammar()
+
+    # swap the probabilities with numbers from 1 to 1000
+    # that way when we generate a radnom number in that range
+    # and easily find out which rule to choose
+    for non_terminal, productions in rules.items():
+        accumulated = 0
+        for i, (probability, production) in enumerate(productions):
+            accumulated = accumulated + probability * 1000
+            productions[i] = (accumulated, production)
+
+    print("DEBUG: Swapping the probabilities with numbers in range[1, 1000]")
+    print_grammar()
 
 def length_without_sapces(s):
     return len(s) - s.count(' ')
@@ -30,9 +85,6 @@ def length_without_sapces(s):
 def generate_language(length, output_filename):
     worklist = [start_symbol]
     non_terminals = rules.keys()
-
-    print("Non-terminals: ", end='')
-    print(*non_terminals, sep=', ')
 
     out = open(output_filename,"w+")
 
@@ -45,7 +97,7 @@ def generate_language(length, output_filename):
             for i, symbol in enumerate(s):
                 if symbol in non_terminals:
                     found_non_terminal = True
-                    for prodcution in rules[symbol]:
+                    for probability_bound, prodcution in rules[symbol]:
                         worklist.append(s[:i] + ' '.join(prodcution) + s[i + 1:])
                     break
 
@@ -53,11 +105,16 @@ def generate_language(length, output_filename):
                 out.write(s + '\n')
                 #print(s)
 
+def get_random_rule(rules):
+    r = random.randint(1, 1000)
+    for probability_bound, rule in rules:
+        if r < probability_bound or math.isclose(r, probability_bound, rel_tol=1e-5):
+            return rule
+    print("Sorry, could not fined a suitable rule with that probabilities, code error...")
+    exit()
+
 def generate_random_words(length, count, output_filename):
     non_terminals = rules.keys()
-
-    print("Non-terminals: ", end='')
-    print(*non_terminals, sep=', ')
 
     out = open(output_filename,"w+")
 
@@ -72,7 +129,7 @@ def generate_random_words(length, count, output_filename):
                 for i, symbol in enumerate(s):
                     if symbol in non_terminals:
                         found_non_terminal = True
-                        rule = random.choice(rules[symbol])
+                        rule = get_random_rule(rules[symbol])
                         worklist.append(s[:i] + ' '.join(rule) + s[i + 1:])
                         break
 
@@ -90,7 +147,16 @@ if __name__ == "__main__":
                       "    F = F & F"
                       "    F = x"
                       "    F = y"
-                      "    F = var")
+                      "    F = var    "
+                      "The grammar can have a probability factor (between 0 and 1, at most 3 digits after the decimal point) "
+                      "for some of it's rules which are going to be used for random word generation. "
+                      "For example: "
+                      "    F 0.5 = F & F"
+                      "    F = x"
+                      "    F = y"
+                      "    F 0.1 = var"
+                      " When resolving the non-terminal F there is 50% chance to choose 'F & F', "
+                      " 10% chance to choose 'var', (auto-calculated) 30 % chance for 'x' and 30% chance for 'y'")
     parser.add_option("-o", "--output_filename", dest="output_filename", type = "string", default = "out_cfg_language.txt",
                       help="Filename for the generated language words")
     parser.add_option("-l", "--length", dest="length", default = 10, type = "int",
@@ -107,11 +173,6 @@ if __name__ == "__main__":
         exit()
 
     load_grammar(options.cfg_filename)
-    print('Loaded grammar start_symbol is ' + start_symbol)
-    print('Rules: ')
-    for lhs in rules:
-        print(lhs + ' = ', end = '')
-        print(*rules[lhs], sep=',')
 
     if options.random_words_count > 0:
         generate_random_words(options.length, options.random_words_count, options.output_filename)
