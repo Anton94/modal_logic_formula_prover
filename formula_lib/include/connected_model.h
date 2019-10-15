a5de824cc8a566f4353a6be64bdb51120d322efe
Anton94 <antonvdudov@gmail.com>
2019-10-15 11:58:33 +0300

    [lib] Start of connected_model
    algorithm (see the header file for more info) for creating a connected model
    
    Still, a couple of things left to implement before it's ready to go

---
+++ formula_lib/include/connected_model.h
@@ -0,0 +1,127 @@
+#pragma once
+
+#include "imodel.h"
+#include "types.h"
+#include "../private/variables_evaluations_block.h"
+
+class formula_mgr;
+class term;
+class formula;
+
+struct connected_model : public imodel
+{
+    using points_t = std::vector<variables_evaluations_block>;
+    /*
+     * Let us have the following formula: C(Ai, Bi) & ~C(Ej, Fj) & Dk!=0 & Gl=0 (i,j,k,l can iterate over some ranges)
+     * Let put some notation: N - number of variables, M - number of atomic formulas
+     * I - number of contacts, J - number of ~C, K - number of != 0 and L - number of =0
+     * 1) Creates all possible unique model points(the model point is just a variable evaluation Pi).
+     *   We have N variables, this are 2^N unique points(evaluations).
+     *   We skip the once which interfere with the zero terms, i.e. we remove all Pi which evaluates some term Gl to constant true.
+     *   We also skip the once which evaluates Ej and Fj to 1 (because of the point's reflexivity it will interfere with ~C atomic formulas)
+     *
+     *   NOTE that if Pi evaluates the term 't' to constant true then the model evaluation v(t) contains the
+     *   point Pi (and vice versa).
+     *
+     * So far, the model satisfies all =0 atomic formulas and 'half' of the ~C (only the reflexivity of the points)
+     *
+     * 2) We check if there is some Pi which evaluates != atomics and contact's terms to true, i.e.
+     *   there is Px which evalutes Ai to 1, Py - Bi to 1 and Pz - Dk to 1 (for all i and k). (Px,Py,Pz can be same or different)
+     *   If there are such evaluations then there is still a chance to find a connected model, otherwise there is no model, not even non-connected one.
+     *
+     * So far, we have need O(2^N * N) bits of memory to hold all points and their evaluations and O(2^N * M) time.
+     *
+     * 3) We build connectivity bit matrix 2^N X 2^N and set all cells to 1 (i.e. we make each point connected to all points)
+     *   O(2^N * 2^N) === O(2^2N) bits
+     *   We remove all connections which interfere with ~C atomic formulas(not that the reflexivity is already satisfied):
+     *     for each a, b < 2^N (each pair of points) and for each j : Pa[Ej]=1 & Pb[Fj]=1 OR Pa[Fj]=1 & Pb[Ej]=1
+     *     then we remove the connection between point 'a' and 'b'
+     *   O(2^N * 2^N * J) time complexity
+     *
+     * 4) We check if C(Ai, Bi) is still satisfied, i.e. we have not removed all connections between v(Ai) and v(Bi)
+     *   for each a, b < 2^N (each pair of points IN CONTACT) and for each i : Pa[Ai]=1 & Pb[Bi]=1 OR Pa[Bi]=1 & Pb[Ai]=1
+     *   O(2^N * 2^N * I) time complexity
+     *
+     * 5) Now we have a 'maximal' unique satisfiable model, which is a sort of graph. We need to find all connected subgraphs in it and if some of them
+     *   is also a satisfiable model then this will be our connected model. Finding all connected subgraphs is a simple tax - O(2^N + 2^N * 2^N), i.e.
+     *   O(V + E) (vertexes + edges).
+     *  Checking if a subset of points satisfies the formula can be done in a similar fashion(as the one above)
+     *  Let's Y is the number of points in the subset, then at most O(Y * Y * M) time complexity.
+     *  Note that we have to do it for each connected subgraph and the subgraphs are not overlapping!
+     *  Upper bound for Y is 2^N, so we can safelty put an upper bound and on the time cimplexity: O(2^N * 2^N * M)
+     *  bacause e.g. x * x * M + y * y * M <<< (x+y)*(x+y)*M
+     *
+     *
+     * Overall memory complexity: O(2^N * 2^N) bits
+     * Overall time complexity: O(2^N * 2^N * M)
+     *
+     * TODO: update the comment with the changes in the implementation
+    */
+
+    auto create(const formulas_t& contacts_T, const formulas_t& contacts_F, const terms_t& zero_terms_T,
+                const terms_t& zero_terms_F, const variables_mask_t& used_variables, const formula_mgr* mgr)
+        -> bool override;
+
+    auto get_model_points() const -> const points_t&;
+
+    auto print(std::ostream& out) const -> std::ostream& override;
+
+    void clear() override;
+
+    ~connected_model() override = default;
+
+private:
+    /*
+     * Constructs all model points(evaluations) which are not breaking the a=0 and ~C(a,b) atomic formulas,
+     * i.e. all Pi (a variable evaluation for all N variables) which are evaluating
+     * all terms 'a' to false and the following is true: !(Pi[a]=1 && Pi[b]=1) === Pi[a]=0 || Pi[b]=0
+     */
+    void construct_all_valid_unique_points(const formulas_t& contacts_F, const terms_t& zero_terms_T);
+    /*
+     * Returns true if evaluation evaluates all zero terms to false
+     */
+    auto are_zero_terms_T_satisfied(const terms_t& zero_terms_T,
+                                    const variables_evaluations_block& evaluation) const -> bool;
+    /*
+     * Returns true if for each ~C(a,b) the following is satisfied:
+     *  !(evaluation evaluates a to true && evaluation evaluates b to true)
+     */
+    auto is_contacts_F_rule_satisfied_only_reflexivity(const formulas_t& contacts_F,
+                                                       const variables_evaluations_block& evaluation) const -> bool;
+
+    /*
+     * Returns true if all non-zero term's model evaluations are not empty.
+     */
+    auto is_zero_terms_F_rule_satisfied(const terms_t& zero_terms_F) const -> bool;
+
+    /*
+     * Returns true if all contact term's model evaluations are not empty.
+     */
+    auto is_contacts_T_existence_rule_satisfied(const formulas_t& contacts_T) const -> bool;
+
+    /*
+     * Returns true if for each C(a,b) there is a contact in @contact_relations_ between v(a) and v(b).
+     */
+    auto is_contacts_T_rule_satisfied(const formulas_t& contacts_T) const -> bool;
+
+    /*
+     * Builds N x N bit matrix with contact relations between all points,
+     * which later reduces in order to satisfy all ~C atomic formulas.
+     * Returns true if the produced contact relations satisfy all contact atomic formulas.
+     */
+    auto build_contact_relations_matrix(const formulas_t& contacts_T, const formulas_t& contacts_F) -> bool;
+
+    /*
+     * Returns a vector of set of points, each of which is a connected component (w.r.t. @contact_relations_)
+     */
+    auto get_connected_components() const -> std::vector<model_points_set_t>;
+
+    void reduce_variable_evaluations_to_subset_of_points(const model_points_set_t& points_subset);
+    void reduce_model_to_subset_of_points(const model_points_set_t& points_subset);
+
+    void calculate_the_model_evaluation_of_each_variable();
+
+    variables_mask_t used_variables_{};
+
+    points_t points_;
+};
---
+++ formula_lib/private/connected_model.cpp
@@ -0,0 +1,304 @@
+#include "connected_model.h"
+#include "formula.h"
+#include "formula_mgr.h"
+#include "term.h"
+
+#include <cassert>
+
+auto connected_model::create(const formulas_t& contacts_T, const formulas_t& contacts_F,
+                             const terms_t& zero_terms_T, const terms_t& zero_terms_F,
+                             const variables_mask_t& used_variables, const formula_mgr* mgr) -> bool
+{
+    clear();
+    used_variables_ = used_variables;
+    mgr_ = mgr;
+
+    construct_all_valid_unique_points(contacts_F, zero_terms_T);
+
+    calculate_the_model_evaluation_of_each_variable();
+
+    if(!is_zero_terms_F_rule_satisfied(zero_terms_F) ||
+       !is_contacts_T_existence_rule_satisfied(contacts_T))
+    {
+        trace() << "Unable to create model points even to satisfy the existence of points in the model evaluation of != 0 and contact terms.";
+        return false;
+    }
+
+    if(!build_contact_relations_matrix(contacts_T, contacts_F))
+    {
+        trace() << "Unable to create contact relations which satisfies ~C and C atomic formulas.";
+        return false;
+    }
+
+    // Now, we have in some sence the biggest model(w.r.t number of unique points and maximal contact relations between them)
+    assert(mgr_->is_model_satisfiable(*this));
+
+    const auto connected_components = get_connected_components();
+
+    const auto original_variable_evaluations = variable_evaluations_; // TODO: consider modifying the checking if the component is a model instead of modifying the variable_evaluations_
+    for(const auto& connected_component : connected_components)
+    {
+        // Restrict the variable evaluations to only those points which are in the @connected_component
+        reduce_variable_evaluations_to_subset_of_points(connected_component);
+
+        if(is_zero_terms_F_rule_satisfied(zero_terms_F) &&
+           is_contacts_T_rule_satisfied(contacts_T))
+        {
+            // Good. The connected component is also a valid model.
+            // Remove all other points(outside the connected component) because we do not need them.
+            reduce_model_to_subset_of_points(connected_component);
+            assert(mgr_->is_model_satisfiable(*this));
+            return true;
+        }
+
+        // rollback the variable_evaluations
+        variable_evaluations_ = original_variable_evaluations;
+    }
+
+    return false;
+}
+
+void connected_model::construct_all_valid_unique_points(const formulas_t& contacts_F,
+                                                        const terms_t& zero_terms_T)
+{
+    variables_evaluations_block evaluation(used_variables_);
+
+    do
+    {
+        if(are_zero_terms_T_satisfied(zero_terms_T, evaluation) &&
+           is_contacts_F_rule_satisfied_only_reflexivity(contacts_F, evaluation))
+        {
+            points_.push_back(evaluation);
+        }
+    } while(evaluation.generate_next_evaluation());
+}
+
+auto connected_model::get_model_points() const -> const points_t&
+{
+    return points_;
+}
+
+void connected_model::clear()
+{
+    used_variables_.clear();
+    points_.clear();
+
+    imodel::clear();
+}
+
+auto connected_model::are_zero_terms_T_satisfied(const terms_t& zero_terms_T,
+                                                 const variables_evaluations_block& evaluation) const -> bool
+{
+    for(const auto& z : zero_terms_T)
+    {
+        if(!z->evaluate(evaluation).is_constant_false())
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+auto connected_model::is_contacts_F_rule_satisfied_only_reflexivity(
+    const formulas_t& contacts_F, const variables_evaluations_block& evaluation) const -> bool
+{
+    for(const auto& c : contacts_F)
+    {
+        const auto a = c->get_left_child_term();
+        const auto b = c->get_right_child_term();
+
+        if(a->evaluate(evaluation).is_constant_true() && b->evaluate(evaluation).is_constant_true())
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+auto connected_model::is_zero_terms_F_rule_satisfied(const terms_t& zero_terms_F) const -> bool
+{
+    for(const auto& z : zero_terms_F)
+    {
+        if(z->evaluate(variable_evaluations_, points_.size()).none())
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+auto connected_model::is_contacts_T_existence_rule_satisfied(const formulas_t& contacts_T) const -> bool
+{
+    for(const auto& c : contacts_T)
+    {
+        const auto a = c->get_left_child_term();
+        const auto b = c->get_right_child_term();
+
+        if(a->evaluate(variable_evaluations_, points_.size()).none() ||
+           b->evaluate(variable_evaluations_, points_.size()).none())
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+auto connected_model::is_contacts_T_rule_satisfied(const formulas_t& contacts_T) const -> bool
+{
+    // C(a,b)
+    for(const auto& c : contacts_T)
+    {
+        const auto v_a = c->get_left_child_term()->evaluate(variable_evaluations_, points_.size());
+        const auto v_b = c->get_left_child_term()->evaluate(variable_evaluations_, points_.size());
+
+        auto point_from_v_a = v_a.find_first();
+        while (point_from_v_a != model_points_set_t::npos)
+        {
+            const auto& contacts_of_point_from_v_a = contact_relations_[point_from_v_a];
+            if ((contacts_of_point_from_v_a & v_b).none())
+            {
+                return false;
+            }
+
+            point_from_v_a = v_a.find_next(point_from_v_a);
+        }
+    }
+    return true;
+}
+
+auto connected_model::build_contact_relations_matrix(const formulas_t& contacts_T, const formulas_t& contacts_F) -> bool
+{
+    const auto point_size = points_.size();
+    contact_relations_.clear();
+    contact_relations_.resize(point_size, model_points_set_t(point_size, true)); // Fill NxN matrix with 1s
+
+    // Removes the connection between each point in v_l and all points from v_r
+    auto connection_remover = [&](auto& v_l, auto& v_r)
+    {
+        const auto neg_v_r = ~v_r;
+
+        auto point_from_v_l = v_l.find_first();
+        while (point_from_v_l != model_points_set_t::npos)
+        {
+            auto& contacts_of_point_from_v_l = contact_relations_[point_from_v_l];
+            contacts_of_point_from_v_l &= neg_v_r; // remove all points in @contacts_of_point_from_v_l which are also in @v_r
+
+            point_from_v_l = v_l.find_next(point_from_v_l);
+        }
+    };
+
+    // ~C(a,b)
+    for(const auto& c : contacts_F)
+    {
+        const auto v_a = c->get_left_child_term()->evaluate(variable_evaluations_, points_.size());
+        const auto v_b = c->get_left_child_term()->evaluate(variable_evaluations_, points_.size());
+
+        assert((v_a & v_b).none()); // no common points, because we built them that way
+
+        connection_remover(v_a, v_b);
+        connection_remover(v_b, v_a);
+    }
+
+    return is_contacts_T_rule_satisfied(contacts_T);
+}
+
+auto connected_model::get_connected_components() const -> std::vector<model_points_set_t>
+{
+    // TODO: implement
+    return {};
+}
+
+void connected_model::reduce_variable_evaluations_to_subset_of_points(const model_points_set_t& points_subset)
+{
+    for(auto& evaluation : variable_evaluations_)
+    {
+        evaluation &= points_subset;
+    }
+}
+
+void connected_model::reduce_model_to_subset_of_points(const model_points_set_t& points_subset)
+{
+    points_t reduced_points;
+    const auto reduced_points_size = points_subset.count();
+    reduced_points.reserve(reduced_points_size);
+    std::unordered_map<size_t, size_t> point_id_old_to_new;
+    point_id_old_to_new.reserve(reduced_points_size);
+
+    auto point = points_subset.find_first();
+    while (point != model_points_set_t::npos)
+    {
+        point_id_old_to_new[point] = reduced_points.size();
+
+        reduced_points.push_back(points_[point]);
+        point = points_subset.find_next(point);
+    }
+
+    // TODO: example, explaining, etc.
+    auto map_subset_of_points_to_reduced_points = [&](const model_points_set_t& points_subset) -> model_points_set_t
+    {
+        model_points_set_t mapped_subset(reduced_points_size); // all 0s
+
+        auto point = points_subset.find_first();
+        while (point != model_points_set_t::npos)
+        {
+            mapped_subset.set(point_id_old_to_new[point]);
+            point = points_subset.find_next(point);
+        }
+        return mapped_subset;
+    };
+
+    contacts_t reduced_contact_relations(reduced_points_size, model_points_set_t(reduced_points_size)); // KxK matrix of 0s (K=@reduced_points_size)
+
+    point = points_subset.find_first();
+    while (point != model_points_set_t::npos)
+    {
+        auto& point_contacts = contact_relations_[point];
+        point_contacts &= points_subset; // remove extra contacts
+
+        const auto reduced_point_id = point_id_old_to_new[point];
+        reduced_contact_relations[reduced_point_id] = map_subset_of_points_to_reduced_points(point_contacts);
+        point = points_subset.find_next(point);
+    }
+
+    points_ = std::move(reduced_points);
+    contact_relations_ = std::move(reduced_contact_relations);
+}
+
+// TODO: common function
+void connected_model::calculate_the_model_evaluation_of_each_variable()
+{
+    const auto points_size = points_.size();
+    variable_evaluations_.clear();
+    variable_evaluations_.resize(
+        mgr_->get_variables().size(),
+        model_points_set_t(points_size)); // initialize each variable evaluation as the empty set
+
+    // Calculate the MODEL evaluation of each variable, i.e. each variable_id
+    // v(Pi) = { point | point_evaluation[Pi] == 1 } , i.e. the evaluation of variable with id Pi is 1, i.e.
+    // the bit at position Pi is 1
+    for(size_t point = 0; point < points_size; ++point)
+    {
+        const auto& point_evaluation = points_[point].get_evaluations();
+
+        // iterate only set bits(1s)
+        auto Pi = point_evaluation.find_first();
+        while(Pi != variables_evaluations_t::npos)
+        {
+            variable_evaluations_[Pi].set(point); // adds the point to the v(Pi) set
+            Pi = point_evaluation.find_next(Pi);
+        }
+    }
+}
+
+auto connected_model::print(std::ostream& out) const -> std::ostream&
+{
+    out << "Model points: \n";
+    for(size_t i = 0; i < points_.size(); ++i)
+    {
+        out << std::to_string(i) << " : ";
+        mgr_->print(out, points_[i]);
+        out << "\n";
+    }
+
+    return out;
+}
