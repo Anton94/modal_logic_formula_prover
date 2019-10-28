9d3a8ceff0bfdb32af641eb83626d36876ccc33c
Anton Dudov <anton.dudov@egt-bg.com>
2019-10-28 09:44:50 +0200

    [formula_proover] Fix the project to not use the json builder

--- formula_proover/source.cpp
+++ formula_proover/source.cpp
@@ -3,11 +3,7 @@
 #include <fstream>
 
 #include "cmd_options/cxxopts.hpp"
-#include "nlohmann_json/json.hpp"
-
-#include "formula_mgr.h"
-#include "model.h"
-#include "logger.h"
+#include "library.h"
 
 using json = nlohmann::json;
 
@@ -17,15 +13,13 @@ int main(int argc, char* argv[])
     set_info_logger([](std::stringstream&& s) { std::cout << s.rdbuf() << std::endl; });
     set_error_logger([](std::stringstream&& s) { std::cout << "ERROR: " << s.rdbuf() << std::endl; });
 
-    error();
     try
     {
         cxxopts::Options options("FormulaProover", "One line description of MyProgram");
 
         options.add_options()
                 ("h,help", "Print help")
-                ("f,formula", "Formula in json fomrat", cxxopts::value<std::string>())
-                ("i,input_file", "File containing a formula in json fomrat", cxxopts::value<std::string>());
+                ("f,formula", "Formula in string fomrat", cxxopts::value<std::string>());
 
         auto result = options.parse(argc, argv);
 
@@ -35,28 +29,16 @@ int main(int argc, char* argv[])
             return 0;
         }
 
-        json formula_json;
+        std::string formula = "C(a,b) & <=(b,c) & C(a,c)";
         if(result.count("formula"))
         {
             auto formula_arg = result["formula"].as<std::string>();
 
-            info() << "Parsing forumla: " << formula_arg;
-            formula_json = json::parse(formula_arg);
-        }
-        if(result.count("input_file"))
-        {
-            auto input_file_path = result["input_file"].as<std::string>();
-            info() << "Reading a formula from an input file: " << input_file_path;
-            std::ifstream in(input_file_path);
-
-            formula_json = json::parse(in);
+            info() << "Read formula: " << formula_arg << " from command line argument.";
         }
 
-        info() << "Parsed into json:\n" << formula_json.dump(4);
-
-        info() << "Building a formula tree with the parsed one...";
         formula_mgr f;
-        info() << "\t" << (f.build(formula_json) ? "success" : "failed") << " " << f;
+        info() << "\t" << (f.build(formula) ? "success" : "failed") << "\n";
 
         info() << "Getting the used variables...";
         const auto variables = f.get_variables();
