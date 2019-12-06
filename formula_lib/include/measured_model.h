2f69177a838ae5b3ee601127a4f9c26d4a266e86
Anton94 <antonvdudov@gmail.com>
2019-12-06 10:11:32 +0200

    [documentation] Start of measured_model notes

--- formula_lib/include/measured_model.h
+++ formula_lib/include/measured_model.h
@@ -13,6 +13,42 @@ class formula;
 struct measured_model : public imodel
 {
     /*
+     * TODO: add O complexities for the algorithm(on each step, etc.)
+     *
+     * Let us have the following formula: C(Ai, Bi) & ~C(Ej, Fj) & Dk!=0 & Gl=0 & <=m(Hs, Os) & ~<=m(Qu, Ru) (i,j,k,l,s,u can iterate over some ranges).
+     * This are all atomic formulas in some path in the tableau.
+     * Let put some notation: N - number of variables, M - number of atomic formulas
+     * I - number of contacts, J - number of ~C, K - number of !=0, L - number of =0, S - number of <=m and U - number of ~<=m
+     *
+     * For each contact C(Ai, Bi) we need two connected model points A and B: A belongs to v(Ai) and B belongs to v(Bi)
+     * (or one point which participates in the evaluation of both terms, but this can be simulated by having the same evaluation for both points, i.e. it's duplicated).
+     * For each Dk!=0 atomic we need one model point which participates in the evaluation.
+     * For each measured atomic we need two points for the both sides of inequality(TODO: explain more here, the system, etc. (maybe just ~<=m needs two points?)
+     * Note that the points can share the same evaluation and in that way we can think of them as the same point but duplicated. (A point is indetified by it's evaluation)
+     *
+     * Each point is a pair of term (Ai, Bi or Dk) and variable evaluation(evaluation for each unique variable in all terms).
+     * Let's call it P with components 't' and 'e', i.e. P(t, e)
+     * The point's evaluation (P[e] evaluates the point's term (P[t]) to 'true' because that way we can garantee that P belongs to v(P[t]).
+     * For example, lets have C(a + b, c * d)
+     * then a valid point for 'a+b' is P(a+b, [1000]), where [1000] is the evaluation for 'a' 'b' 'c' 'd' variables. That evaluation evaluates 'a+b' to true.
+     * For 'c*d' a point could be: P('c*d', [0011]).
+     * We need the point to know it's associated term because we will generate 'next' evaluation which evaluates the term to 'true'.
+     *
+     * 1) For each C(Ai, Bi) we create two points Pa and Pb: Pa[Ai] = true and Pb[Bi] = true. (if not possible then there is no such model, at each step, if we can't create next point then there is no such model)
+     * 2) For each Dk!=0 we create one point P: P[Dk] = true
+     *
+     * 3) If the points are less than the measured atomics*2 (or zero points) then we create dummy points which are associated to '1' constant term, just to make real points and to be able to put at least one variable in both sides of the inequalities
+     *
+     * Now, We have at least one point.
+     *
+     * 4) We make a connection between the contact points, i.e. for each 0 <= i < I we add a connection in the connectivity matrix between 2i and 2i + 1 points.
+     * 5) We calculate the v(X) for each variable. (TODO: this should be explained somewhere else already)
+     *
+     * Now, we have satisfied all C(Ai, Bi) and Dk!=0 atomic formulas.
+     *
+     * 6)
+     *
+     *
      * The main difference between this model and 'model.h' is that this one generates new points and checks some conditions
      * and the other once generates the points in a way which satisfies the conditions and every point(or pair of points for contacts)
      * which it generates are satisfiable and there is no need to go back and regenrate them.
