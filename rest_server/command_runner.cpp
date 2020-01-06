a15aec90d9694863656a34da42c2d72b35265338
Anton94 <antonvdudov@gmail.com>
2020-01-06 14:55:20 +0200

    [rest_server] Add command runner API
    used for run a commands from C++. Will use it to run python scripts for formula generations, but it could run any command

---
+++ rest_server/command_runner.cpp
@@ -0,0 +1,31 @@
+#include "command_runner.h"
+
+#include <cstdio>
+
+namespace command_runner
+{
+
+void read_output(FILE* file, std::string& output)
+{
+    char byte{0};
+    while(fread(&byte, 1, 1, file))
+    {
+        output.push_back(byte);
+    }
+}
+
+bool run(const std::string& cmd, std::string& output)
+{
+    auto cmd_with_redirected_error_output = cmd + " 2>&1";
+    FILE* file = popen(cmd_with_redirected_error_output.c_str(), "r");
+    if(!file)
+    {
+        return false;
+    }
+
+    read_output(file, output);
+
+    return pclose(file) == 0;
+}
+
+}
---
+++ rest_server/command_runner.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+
+namespace command_runner
+{
+
+/// A blocking operation which executes @cmd and waits for it to finish. Returns true if @cmd successfully finishes.
+/// Writes the produced output to @output.
+/// Note that the error output is redirected, so an '2>&1' is appended to the command (@cmd)!
+/// Example: "ls -la"
+bool run(const std::string& cmd, std::string& output);
+
+}
