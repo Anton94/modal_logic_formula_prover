a8b3cb571275ae97ae8db979b273af0766a2058f
Anton94 <antonvdudov@gmail.com>
2020-01-30 13:07:54 +0200

    [rest_server] Disable command runner
    not used at the moment and not cross platform

--- rest_server/command_runner.cpp
+++ rest_server/command_runner.cpp
@@ -1,5 +1,6 @@
 #include "command_runner.h"
 
+#include <cassert>
 #include <cstdio>
 
 namespace command_runner
@@ -7,27 +8,26 @@ namespace command_runner
 
 void read_output(FILE* file, std::string& output)
 {
-    char byte{0};
-    while(fread(&byte, 1, 1, file))
-    {
-        output.push_back(byte);
-    }
+//    char byte{0};
+//    while(fread(&byte, 1, 1, file))
+//    {
+//        output.push_back(byte);
+//    }
 }
 
 bool run(const std::string& cmd, std::string& output)
 {
-#ifdef __WIN32__
-#define popen  _popen
-#endif
-    auto cmd_with_redirected_error_output = cmd + " 2>&1";
-    FILE* file = popen(cmd_with_redirected_error_output.c_str(), "r");
-    if(!file)
-    {
-        return false;
-    }
+    assert(false && "Not implemented");
+    return false;
+//    auto cmd_with_redirected_error_output = cmd + " 2>&1";
+//    FILE* file = popen(cmd_with_redirected_error_output.c_str(), "r");
+//    if(!file)
+//    {
+//        return false;
+//    }
 
-    read_output(file, output);
+//    read_output(file, output);
 
-    return pclose(file) == 0;
+//    return pclose(file) == 0;
 }
 }
